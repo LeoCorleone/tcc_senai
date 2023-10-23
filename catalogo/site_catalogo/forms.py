115899c9e4c72d36471c6290bea7b45370220a8c
Rebeca <rebecavelasco2020@gmail.com>
2023-10-23 20:40:08 -0300

    começando a criar o form PostagemForms

--- catalogo/site_catalogo/forms.py
+++ catalogo/site_catalogo/forms.py
@@ -1,1 +1,28 @@
 from django import forms
+
+class PostagemForms(forms.Form):
+    titulo = forms.CharField(
+        label = 'Titulo',
+        required = True,
+        max_length = 30,
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'form-control',
+                'placeholder': 'Digite o titulo da postagem'
+            }
+        )
+    )
+    descricao = forms.CharField(
+        label = 'Descrição',
+        required = True,
+        max_length = 200,
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'form-control',
+                'placeholder': 'Digite a descrição da postagem'
+            }
+        )
+    )
+    imagem = forms.ImageField(
+        
+    )
