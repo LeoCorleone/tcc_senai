d7ae08bcddb7e37c19231dd1dddb29fbdb256e58
Rebeca <rebecavelasco2020@gmail.com>
2023-10-23 21:43:27 -0300

    criando database catalogo

--- catalogo/setup/settings.py
+++ catalogo/setup/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'catalogo',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
