400157c62b7e171f869e45103f85603505e76f19
Rebeca <rebecavelasco2020@gmail.com>
2023-10-24 21:40:32 -0300

    teste

--- catalogo/site_catalogo/urls.py
+++ catalogo/site_catalogo/urls.py
@@ -9,15 +9,10 @@ from django.conf.urls.static import static
 from . import views 
 
 urlpatterns = [
-<<<<<<< HEAD
     path("", views.index, name="index"),
     path("adm/", views.adm, name= "adm"),
-=======
-    path("", index, name="index"),
-    path("adm", adm, name= "adm"),
->>>>>>> ccc2c4e42d70d71e981c0d0dbd96bf83e3c05420
     path('postagem/', postagem, name = 'postagem'),
-    path('login/', login, name='login')
+    # path('login/', views.login, name='login')
 ]
 
 # if settings.DEBUG:
