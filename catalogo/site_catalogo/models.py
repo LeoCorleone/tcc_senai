b3aa0290f42ae4bcf92a27625858d2f80d415b30
LeleoCorleone <120337467+LeleoCorleone@users.noreply.github.com>
2023-11-26 14:16:20 -0300

    bd novo adicionado

--- catalogo/site_catalogo/models.py
+++ catalogo/site_catalogo/models.py
@@ -34,3 +34,36 @@ class Comentario(models.Model):
     data_publicacao = models.DateTimeField(auto_now_add=True)
 
 
+
+# class Ano(models.Model):
+#     valor = models.IntegerField()
+
+# class Colecao(models.Model):
+#     nome = models.CharField(max_length=255)
+
+# class Tipo(models.Model):
+#     nome = models.CharField(max_length=255)
+
+# class Produto(models.Model):
+#     titulo_produto = models.CharField(max_length=50)
+#     descricao_produto = models.CharField(max_length=250)
+#     composicao_produto = models.CharField(max_length=100)
+#     imagem_produto = models.ImageField(upload_to='uploads/')
+#     modelo = models.ForeignKey("Modelo", on_delete=models.CASCADE, related_name='produtos')
+#     ano = models.ForeignKey(Ano, on_delete=models.CASCADE)
+#     colecao = models.ForeignKey(Colecao, on_delete=models.CASCADE)
+#     tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
+#     likes = models.PositiveIntegerField(default=0)
+#     liked_by = models.ManyToManyField(User, blank=True)
+
+# class Modelo(models.Model):
+#     nome_modelo = models.CharField(max_length=50)
+
+#     def __str__(self):
+#         return self.nome_modelo
+
+# class Comentario(models.Model):
+#     texto = models.TextField()
+#     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+#     produto = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name="comentarios")
+#     data_publicacao = models.DateTimeField(auto_now_add=True)
