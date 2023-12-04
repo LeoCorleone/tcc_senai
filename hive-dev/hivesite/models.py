aa153aef58d8dd230e74fac64d485f33c7a6b9d7
Guilherme <guiassishg11@gmail.com>
2023-12-04 19:14:43 -0300

    creation of migrations

--- hive-dev/hivesite/models.py
+++ hive-dev/hivesite/models.py
@@ -1,3 +1,11 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+class Site(models.Model):
+    nome_card = models.CharField(max_length=200)
+    description =  models.TextField()
+    path = models.TextField()
+    primeiro_texto = models.TextField()
+    segundo_texto = models.TextField()
+    date_create = models.DateTimeField(default=datetime.now, blank =True)
