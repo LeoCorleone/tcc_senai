11bb742c3236224cb9db181b841a219e8dfb11be
Leonardo Corleone <leocunha25@yahoo.com.br>
2023-12-07 19:10:57 -0300

    criando o Fale Conosco

--- catalogo/site_catalogo/models.py
+++ catalogo/site_catalogo/models.py
@@ -40,3 +40,7 @@ class Comentario(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name="comentarios")
     data_publicacao = models.DateTimeField(auto_now_add=True)
+
+class FaleConosco(models.Model):
+    email = models.TextField()
+    texto = models.TextField()
